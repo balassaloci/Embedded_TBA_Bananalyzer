62a3e344d1509c7a641719f448c22d0f16295fc4
davidanderle <anderle.david@gmail.com>
2017-02-09 22:50:55 +0000

    Add files via upload
    Added interrupt option to RGBC reading and code structure redesigned

---
+++ tcstest2.py
@@ -0,0 +1,70 @@
+import tcs34725
+from machine import I2C, Pin, unique_id
+import time
+import network
+from umqtt.simple import MQTTClient
+#----------------------------------------I/O pins---------------------------------------------------
+allowReadPin = machine.Pin(0, Pin.IN, None)
+allowReadPin.irq(trigger = Pin.IRQ_RISING, handler = allowRead_callback)
+allowReadPin.irq(trigger = Pin.IRQ_FALLING, handler = disableRead_callback)
+allowRead = False
+
+def allowRead_callback:
+    allowRead = True
+
+def disableRead_callback:
+    allowRead = False
+#----------------------------------------WiFi setup-------------------------------------------------
+labNetwork = 'EEERover'
+labPassword = 'exhibition'
+brokerAddress = '192.168.0.10'
+deviceName = 'esp8266_' + str(unique_id(), 'utf-8')
+
+connectWiFi(labNetwork, labPassword)
+
+def connectWiFi(ssid, password):
+    wlan = network.WLAN(network.STA_IF)                 # create station interface
+    wlan.active(True)                                   # activate the interface
+    if not wlan.isconnected():                          # check if the station is connected to an AP
+        wlan.connect(ssid, password)                    # connect to an AP
+        while not wlan.isconnected():
+            print('.', end = '')
+            time.sleep_ms(500)
+        print('Connected!')
+    wlan.config('mac')                                  # get the interface's MAC adddress
+
+    print('Network config', wlan.ifconfig())
+#----------------------------------------MQTT client setup------------------------------------------
+client = MQTTClient(deviceName, brokerAddress)
+client.set_callback(sub_cb)
+client.connect()
+client.subscribe('esys/TBA/sensor')
+
+def sub_cb(topic,msg):
+    print(msg)
+#----------------------------------------Sensor setup-----------------------------------------------
+i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
+sensor = tcs34725.TCS34725(i2c)
+sensor.gain(16)
+
+def convert_rgb_data(raw_rgb):
+    corrected_data = (raw_rgb[0]/0.88,raw_rgb[1]/0.66, raw_rgb[2]/0.57,raw_rgb[3]/1)
+    const = 255/1024
+    corrected_data = raw_rgb
+    corrected_data = (  int(round(corrected_data[0]*const, 0)), 
+                        int(round(corrected_data[1]*const, 0)),
+                        int(round(corrected_data[2]*const, 0)),
+                        int(round(corrected_data[3]*const, 0)))
+    return corrected_data
+#----------------------------------------Data reading-----------------------------------------------
+while allowRead == True:
+    s = sensor.read(True)
+    s = convert_rgb_data(s)
+    #print(s)
+    client.publish('esys/TBA/sensor', 'RGBC: (' 
+            + str(s[1]) + ', '+ str(s[1]) + ', ' + str(s[2]) + ', ' + str(s[3]) +')')
+    time.sleep_ms(100)
+    client.wait_msg()
+
+sensor.active(False)
+client.disconnect()
