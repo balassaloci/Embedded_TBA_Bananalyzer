6085814ddc5cfb973d872a1445024ab3a35c6a6b
davidpasztor <pasztor.david95@hotmail.com>
2017-02-15 16:08:46 +0000

    device code refactored

--- device/test.py
+++ device/test.py
@@ -5,30 +5,19 @@ import time
 from machine import I2C, Pin, unique_id, RTC
 from umqtt.simple import MQTTClient
 
-date = 0, 0, 0, 0, 0, 0, 0, 0           # set global variable
-payload =""
+date = 0, 0, 0, 0, 0, 0, 0, 0           # set global variables for date and
+payload =""                             # JSON payload
 #----------------------------------------I/O pins-------------------------------
 allowReadPin = Pin(14, Pin.IN, None)    # Switch input pin14 without pull res.
-#----------------------------------------Time parser----------------------------
+#----------------------------------------Date parser----------------------------
 def parseDate(a):
     return  int(a[0:4]), int(a[5:7]),   \
             int(a[8:10]), int(a[11:13]),\
-            int(a[14:16]), int(a[17:19]), 0, 0 
-#----------------------------------------Data uploading-------------------------
-def uploadData():
-    with open('JSONData.txt', 'r') as savedData:
-        JSONData = savedData.read()
-        client.publish('esys/TBA/sensor/data', JSONData)
-    with open('JSONData.txt', 'w') as savedData:
-        pass
+            int(a[14:16]), int(a[17:19]), 0, 0
 #----------------------------------------MQTT client setup----------------------
-deviceName = 'esp8266_' + str(unique_id(), 'utf-8')
-brokerAddress = '192.168.0.10'
-
-def sub_cb(topic,msg):                  # callback for debu
-    print(msg)                          #debug
+def sub_cb(topic,msg):                  # callback for wait_msg()
+    print(msg)
     if topic == b'esys/TBA/sensor/control1' and msg == b'upload':
-        #uploadData()
         global payload
         client.publish('esys/TBA/sensor/data', payload)
         print("Data sent")
@@ -41,6 +30,8 @@ def sub_cb(topic,msg):                  # callback for debu
         print(date)                     #debug
         print('set as system time')
 
+deviceName = 'esp8266_' + str(unique_id(), 'utf-8')
+brokerAddress = '192.168.0.10'
 client = MQTTClient(deviceName, brokerAddress)
 client.set_callback(sub_cb)
 #----------------------------------------Internal clock setup-------------------
@@ -55,25 +46,26 @@ rtc.datetime((date))                    #set internal clock to server time
 #----------------------------------------RGB Sensor setup-----------------------
 i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
 sensor = tcs34725.TCS34725(i2c)
-sensor.gain(16)                         #gain: 1, 2, 16, 60 
-#sensor.integration_time(402)           #change this for darker light conditions
-
-def convert_rgb_data(raw_rgb):
-    const = 255/1024                    #Convert 10 bit ADC data to 8 bit RGB
+sensor.gain(16)                         #gain: 1, 2, 16, 60
+#----------------------------------------Data conversion------------------------
+def convert_rgb_data(raw_rgb):          #Convert 10 bit ADC data to 8 bit RGB
+    const = 255/1024
     raw_rgb = (  int(round(raw_rgb[0]*const, 0)), 
                  int(round(raw_rgb[1]*const, 0)),
                  int(round(raw_rgb[2]*const, 0)))
     return rgb2hsl.rgb2hsl(raw_rgb[0], raw_rgb[1], raw_rgb[2])
-def getColorName(hsl):
+
+def getColorName(hsl):                  #Convert HSL code to colour names
     if (38<=hsl[0]<=61) and (30<=hsl[1]<=100):
-        return 'Yellow'
+        return 'Yellow'                 #ranges are based on empirical data
     elif (80<=hsl[0]<=150) and (20<=hsl[1]<=60):
         return 'Green'
     elif (13<=hsl[0]<=36) and (30<=hsl[1]<=60):
         return 'Brown'
     else:
-        return 'Non-valid banana', 0
-def getRipeness(hsl):
+        return 'Non-valid banana'
+
+def getRipeness(hsl):               #map the used colour range to a percentage
     if (80<=hsl[0]<=150) and (20<=hsl[1]<=60):
         percent = int(round(100/116*(hsl[0]-80) + 39.6, 0))
         return percent
@@ -85,29 +77,25 @@ def getRipeness(hsl):
         return percent
     else:
         return 0
-
 #----------------------------------------Data reading and storing---------------
 while True:
     if allowReadPin.value() == 1:           #If switch is on, read values
-        s = sensor.read(True)               #Allow sensor read 
+        s = sensor.read(True)               #Get sensor reading
         raw_rgb = (s[0], s[1], s[2])
         hsl = convert_rgb_data(raw_rgb)     #Convert obtained data
         print(hsl)
         colorName = getColorName(hsl)
-        ripeness = getRipeness(hsl)
         print(colorName)
+        ripeness = getRipeness(hsl)
         print(ripeness)
+        #convert data to JSON before uploading it to server
         payload = json.dumps({'HSL Data':{'H':hsl[0],'S':hsl[1],'L':hsl[2]},
                                 'Banana color': colorName, 
                                 'Ripeness': ripeness,
                                 'Time': rtc.datetime()})
-
-        #dataToUpload = open('JSONData.txt', 'a')
-        #dataToUpload.write(payload)
-        #dataToUpload.close()
         
         client.connect()                    #Connect to MQTT server
         client.subscribe('esys/TBA/sensor/control1')
-        client.wait_msg()
+        client.wait_msg()                   #wait for upload message from server
         time.sleep_ms(100)
         client.disconnect()                 #Disconnect from the server
